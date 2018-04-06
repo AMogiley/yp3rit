6f009902a267a2f0f9f3ebdca8ee71ff2be8aead
mCstl <37896336+mCstl@users.noreply.github.com>
2018-04-06 14:18:45 +0300

    Update yp3rit.rb

--- yp3rit.rb
+++ yp3rit.rb
@@ -4,10 +4,10 @@ require 'open-uri'
 require 'nokogiri'
 require 'shodan'
 
-print ("\n\n   ___________________________________________________________________________________________   \n   |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||   \n   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   \n   |~~~~~~~~~~~~~~~~~~~~~~##~~##~~#####~~~~####~~~#####~~~######~~######~~~~~~~~~~~~~~~~~~~~~|   \n   |~~~~~~~~######~~##~~~~~####~~~##~~##~~#~~~##~~##~~##~~~~##~~~~##~~~~~~##~~~~######~~~~~~~|   \n   |#####~~~~~~~~~~~~~~~~~~~##~~~~#####~~~~~###~~~#####~~~~~##~~~~#####~~~~~~~~~~~~~~~~~#####|   \n   |~~~~~~~~######~~##~~~~~~##~~~~##~~~~~~#~~~##~~##~~##~~~~##~~~~##~~~~~~##~~~~######~~~~~~~|   \n   |~~~~~~~~~~~~~~~~~~~~~~~~##~~~~##~~~~~~~####~~~##~~##~~~~##~~~~######~~~~~~~~~~~~~~~~~~~~~|   \n   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   \n   |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||   \n   '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''   \n\n")
+print (" \n\n  \x1b[40m\x1b[1m\x1b[37m______________________________________________________________________________________________\x1b[40m\x1b[0m  \n  \x1b[40m\x1b[1m\x1b[37m||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\x1b[40m\x1b[0m  \n  \x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[1m\x1b[37m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[0m  \n  \x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[1m\x1b[37m~~~~~~~~~~~~~~~~~~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m#####\x1b[40m\x1b[1m\x1b[37m~~~~\x1b[40m\x1b[1m\x1b[33m####\x1b[40m\x1b[1m\x1b[37m~~~\x1b[40m\x1b[1m\x1b[33m#####\x1b[40m\x1b[1m\x1b[37m~~~\x1b[40m\x1b[1m\x1b[33m######\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m########\x1b[40m\x1b[1m\x1b[37m~~~~~~~~~~~~~~~~~~~~~~\x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[0m  \n  \x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[1m\x1b[37m~~~~~~~~\x1b[40m\x1b[1m\x1b[33m######\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~\x1b[40m\x1b[1m\x1b[33m####\x1b[40m\x1b[1m\x1b[37m~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m#\x1b[40m\x1b[1m\x1b[37m~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~\x1b[40m\x1b[1m\x1b[33m######\x1b[40m\x1b[1m\x1b[37m~~~~~~~\x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[0m  \n  \x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[40m\x1b[1m\x1b[33m#####\x1b[40m\x1b[1m\x1b[37m~~~~~~~~~~~~~~~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~\x1b[40m\x1b[1m\x1b[33m#####\x1b[40m\x1b[1m\x1b[37m~~~~~\x1b[40m\x1b[1m\x1b[33m###\x1b[40m\x1b[1m\x1b[37m~~~\x1b[40m\x1b[1m\x1b[33m#####\x1b[40m\x1b[1m\x1b[37m~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~~~~~~~~~~~~~~~~\x1b[40m\x1b[1m\x1b[33m#####\x1b[40m\x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[0m  \n  \x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[1m\x1b[37m~~~~~~~~\x1b[40m\x1b[1m\x1b[33m######\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~~\x1b[40m\x1b[1m\x1b[33m#\x1b[40m\x1b[1m\x1b[37m~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~\x1b[40m\x1b[1m\x1b[33m######\x1b[40m\x1b[1m\x1b[37m~~~~~~~\x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[0m  \n  \x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[1m\x1b[37m~~~~~~~~~~~~~~~~~~~~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~~~\x1b[40m\x1b[1m\x1b[33m####\x1b[40m\x1b[1m\x1b[37m~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~\x1b[40m\x1b[1m\x1b[33m######\x1b[40m\x1b[1m\x1b[37m~~~~~\x1b[40m\x1b[1m\x1b[33m##\x1b[40m\x1b[1m\x1b[37m~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[0m  \n  \x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[1m\x1b[37m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[40m\x1b[1m\x1b[37m|\x1b[40m\x1b[0m  \n  \x1b[40m\x1b[1m\x1b[37m||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\x1b[40m\x1b[0m  \n  \x1b[40m\x1b[1m\x1b[37m''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''\x1b[0m  \n")
 $lin="================================================================================"
 	def whiche
-		print ("\n\n  You may try to search it by masscan or nmap \n (m) masscan \n (n) nmap  \n (s) Shodan \n\n >")
+		print ("\n\n  You may try to search it by masscan or nmap \n\n  (m) masscan \n  (n) nmap  \n  (s) Shodan \n\n >")
 		prog= gets().chomp()
 		if prog	== "masscan" || prog == "m" then
 			masscan
@@ -15,18 +15,19 @@ $lin="==========================================================================
 			nmap
 		elsif prog == "shodan" || prog == "s"
 			shodan
+		elsif prog == "exit" || prog == "e"
+			abort
 		end
 	end
 
 	def shodan
 		print ("\n"+$lin+"\n")
-		print ($lin+"\n\n")
-		print ("\n\n\n\n\n\n\n Ok, you can try to test \n (f) Test honypot ip's file \n (h) Test target host on honypot \n (s) Shodan search \n\n")
+		print ("\n Ok, you can try to test \n (f) Test honypot ip's file \n (h) Test target host on honypot \n (s) Shodan search \n\n")
 		print (" --> ")
 		way = gets().chomp()
 		if way == "f" then
 			honyf
-		elsif way == "t"
+		elsif way == "h"
 			honyt
 		elsif way == "s"
 			shod
@@ -40,10 +41,11 @@ $lin="==========================================================================
 			key= ("WA9rlYKJpojwY8DUeLwbz9sESG5m8YRc");		
 			print ("\n FILE > ")
 			input= gets().chomp()
-			print ("\n OUTPUT > ")
+			print (" file => #{input} \n\n OUTPUT > ")
 			oput= gets().chomp()
-			print ("\n RISK > ")
+			print (" output => #{oput} \n\n RISK > ")
 			risk= gets().chomp()
+			print (" risk => #{risk} \n")
 			file = File.open("#{input}")
 			file.each {|line| x = Nokogiri::HTML(open("https://api.shodan.io/labs/honeyscore/#{line.chomp}?key=#{key}")).xpath("//text()").text.chomp
 				if x<=risk then
@@ -61,8 +63,9 @@ $lin="==========================================================================
 			STDOUT.flush;
 			print ("\n RHOST > ");
 			ip= gets().chomp();
-			print ("\n RISK > ")
+			print (" rhost => #{ip} \n\n RISK > ")
 			risk= gets().chomp()
+			print (" risk => #{risk} \n\n")
 			key= ("WA9rlYKJpojwY8DUeLwbz9sESG5m8YRc");
 			doc = Nokogiri::HTML(open("https://api.shodan.io/labs/honeyscore/#{ip}?key=#{key}"));
 			x = (doc.xpath("//text()").text)
@@ -75,28 +78,35 @@ $lin="==========================================================================
 
 		def shod
 			api = Shodan::Shodan.new('WA9rlYKJpojwY8DUeLwbz9sESG5m8YRc')
-			print ("Query > ")
+			print (" QUERY > ")
 			STDOUT.flush
-			qry	= gets.chomp
+			qry = gets.chomp
+			print (" query => #{qry} \n\n OUTPUT > ")
+			oput = gets().chomp()
+			print (" output => #{oput} \n\n")
 			result = api.search(qry)
 			result['matches'].each{ |host|
 			        puts host['ip_str']
+				ip = (host['ip_str'])
+				ofile = File.new("#{oput}", 'a')			
+				print ("#{ip}\n")
+				ofile.print ("#{ip.chomp}\n")
+				ofile.close
 			}
 		shodan
 		end
 
 	def masscan
 		print ("\n"+$lin+"\n\n")
-		puts $lin
-		print ("\n\n  MASSCAN \n\n   So now you will chose port and ip diapason file, \n  put the nomber of the port (usally 3306) \n  and stsic link to the file.\n\n FILE > ")
+		print ("\n\n  MASSCAN \n\n   So now you will chose port and ip diapason file, \n  put the nomber of the port (usally 3306) \n  and stsic link to the file.\n\n INPUT > ")
 		file = gets().chomp()
-		print (" --> "+file+"\n\n RPORT > ")
+		print (" input => #{file} \n\n RPORT > ")
 		port = gets().chomp()
-		print (" --> "+port+" \n\nNow you may choose other options for masscan, or just press enter\n\n OPTIONS > ")
+		print (" rport => #{port} \n\n OPTIONS > ")
 		opt = gets().chomp()
-		print (" --> "+opt+"\n\n Put name of output file \n\n OUTPUT > ")
+		print (" options => #{opt} \n\n OUTPUT > ")
 		outf = gets().chomp()
-		print (" --> "+outf+"\n\n\n Ok, now just type run, and it good to go \n or operation will start all over \n\n RUN > ")
+		print (" output => #{outf} \n\n RUN > ")
 		run = gets().chomp()
 		if run == "run" || run == "RUN" then
 			system ("masscan #{opt} -p#{port} --randomize-hosts -Pn -n -sS --connection-timeout 20  --open -iL #{file} | grep -E -o '(([0-9]{1,3}[\.]){3}[0-9]{1,3})' > #{outf}")
@@ -108,15 +118,16 @@ $lin="==========================================================================
 			puts ("==============================")
 			print ("\n\n        !  #{host_count} hosts\n\n")
 			puts ("==============================")
-		
+		elsif run == "back" || run == "b" then		
+			whiche
 		else masscan
 		end
 	end
 
 	def nmap
 	def which
-		print ("\n"+$lin+"\n\n")
-		print ("\n\n\nWhat aru you going to test: \n\n  (1) FTP \n  (2) SSH \n  (3) SSL \n  (4) VULN \n  (5) MySQL \n  (u) Unusual-port \n     on port put number of ports \n  (t) Scan target without ports \n (b) Back   \n\n  > ")
+		print ("\n"+$lin+"\n")
+		print ("\nWhat aru you going to test: \n\n  (1) FTP \n  (2) SSH \n  (3) SSL \n  (4) VULN \n  (5) MySQL \n  (u) Unusual-port (number of top ports) \n  (t) Scan target without ports \n  (b) Back   \n\n  > ")
 		prog= gets().chomp()
 		print ("\n\n  RHOST > ")
 		@rhosts= gets().chomp()
@@ -125,15 +136,15 @@ $lin="==========================================================================
 		print ("  rport => "+@rports+"\n\n  OPTIONS > ")
 		@opts= gets().chomp()
 		print ("  options => "+@opts+"\n\n")
-		if prog	== "1" || prog == "m" then
+		if prog	== "1" || prog == "ftp" then
 			ftp
-		elsif prog == "2" || prog == "n"
+		elsif prog == "2" || prog == "ssh"
 			ssh
-		elsif prog == "shodan" || prog == "s"
+		elsif prog == "3" || prog == "ssl"
 			ssl
-		elsif prog == "2" || prog == "n"
+		elsif prog == "4" || prog == "vuln"
 			vuln
-		elsif prog == "2" || prog == "n"
+		elsif prog == "5" || prog == "mysql"
 			mysql
 		elsif prog == "u" || prog == "unusual"
 			system ("nmap --top-ports #{@rports} #{opt} --script unusual-port #{host}")
@@ -143,12 +154,11 @@ $lin="==========================================================================
 			which
 		elsif prog == "b" || prog == "back"
 			whiche
-#				system ("nmap --top-ports #{@rports} #{opt} --script unusual-port #{host}")
-#				system ("nmap -p#{@rports} #{@opts} #{@rhosts}")
+
 		end
 	end
 			def ftp
-				print ("\n (1) Finde FTP on unusual-port \n (2) Scan target ftp \n (3) Firewall-bypass with FTP helper \n (4) CVE-2010-1938 - OPIE off-by-one stack overflow \n (5) Stack-based buffer overflow in the \n      ProFTPD server 1.3.2rc3 - 1.3.3b \n (6) Tests ProFTPD 1.3.3c for backdoor \n (7) Tests vsFTPd 2.3.4 for backdoor \n (0) Exit \n (s) Back \n\n")
+				print ("\n (1) Finde FTP on unusual-port \n (2) Scan target ftp \n (3) Firewall-bypass with FTP helper \n (4) CVE-2010-1938 - OPIE off-by-one stack overflow \n (5) Stack-based buffer overflow in the \n     (ProFTPD server 1.3.2rc3 - 1.3.3b) \n (6) Tests ProFTPD 1.3.3c for backdoor \n (7) Tests vsFTPd 2.3.4 for backdoor \n (0) Exit \n (b) Back \n\n")
 				print ("\n\n  RUN > ")
 				runs= gets().chomp()		
 				
@@ -191,7 +201,7 @@ $lin="==========================================================================
 						system ("nmap -p#{@rports} --script ftp-brute  #{@rhosts}")				
 						ftp
 		
-					elsif runs == "start" || runs == "s"
+					elsif runs == "back" || runs == "b"
 		
 						which
 					
@@ -205,7 +215,7 @@ $lin="==========================================================================
 			end
 		
 			def ssh
-				print ("\n (1)  Check SSH Protocol Version 1 \n (2) Report number of algorithms (for encryption, compression, etc.) of SSH2 server \n (3) Returns authentication methods \n (4) Show the target server's key fingerprint  \n (5) Run remote command on server \n (6) Brute \n (0) Exit \n (s) Back  \n\n")
+				print ("\n (1) Check SSH Protocol Version 1 \n (2) Report number of algorithms (for encryption, compression, etc.) of SSH2 server \n (3) Returns authentication methods \n (4) Show the target server's key fingerprint  \n (5) Run remote command on server \n (6) Brute \n (0) Exit \n (s) Back  \n\n")
 				print ("\n\n  RUN > ")
 				runs= gets().chomp()		
 				
@@ -252,7 +262,7 @@ $lin="==========================================================================
 						system ("nmap -p#{@rports} #{@opts} --script ssh-brute --script-args userdb=users.lst,passdb=pass.lst --script-args ssh-brute.timeout=4s #{@rhosts}")
 						ssh
 		
-					elsif runs == "start" || runs == "s"
+					elsif runs == "back" || runs == "b"
 		
 						which
 					
@@ -268,8 +278,9 @@ $lin="==========================================================================
 		
 			def ssl
 		
-				print ("\n (1) Scan target\n (2) CVE-2014-0224 \n      File SSL CSS-injection\n (3) SSL cert-intaddr IP.4  \n (4) Retrieves host's time and date from its TLS ServerHello response \n (5) CVE 2015-4000  \n     Weak ephemeral Diffie-Hellman parameter detection for SSL/TLS services \n (0) Exit \n (s) Back  \n\n")
-		
+				print ("\n (1) Scan target\n (2) CVE-2014-0224 \n      File SSL CSS-injection\n (3) SSL cert-intaddr IP.4  \n (4) Retrieves host's time and date from its TLS ServerHello response \n (5) CVE 2015-4000  \n     Weak ephemeral Diffie-Hellman parameter detection for SSL/TLS services \n (0) Exit \n (b) Back  \n\n")
+				print ("\n\n  RUN > ")
+				runs= gets().chomp()
 				if runs == "1" then
 				
 					system ("nmap #{@opts} --script unusual-port #{@rhosts}")
@@ -309,7 +320,7 @@ $lin="==========================================================================
 					system ("nmap -p#{@rports} --script ftp-brute  #{@rhosts}")				
 					ssl
 	
-				elsif runs == "start" || runs == "s"
+				elsif runs == "back" || runs == "b"
 	
 					which
 				
@@ -324,7 +335,7 @@ $lin="==========================================================================
 	
 		
 			def vuln
-				print ("\n (1) Use Nmap-Vulners \n (2) Use Vulscan all \n (3) scipvuldb.csv \n (4) exploitdb.csv \n (5) securitytracker.csv \n (0) Exit \n (s) Back   \n\n")
+				print ("\n (1) Use Nmap-Vulners \n (2) Use Vulscan all \n (3) scipvuldb.csv \n (4) exploitdb.csv \n (5) securitytracker.csv \n (0) Exit \n (b) Back   \n\n")
 				print ("\n\n  RUN > ")
 				runs= gets().chomp()		
 				
@@ -344,7 +355,7 @@ $lin="==========================================================================
 					elsif runs =="5"
 						system ("nmap -p#{@rports} --script vulscan --script-args vulscandb=securitytracker.csv -sV  #{@opts} #{@rhosts}")
 						vuln
-					elsif runs == "start" || runs == "s"
+					elsif runs == "back" || runs == "b"
 						which
 					elsif runs == "exit" || runs == "0"	
 						abort
@@ -354,7 +365,7 @@ $lin="==========================================================================
 	
 	
 			def mysql
-				print ("\n (1) Get info \n (2) List all users on server \n (3) Perform valid-user enumeration \n (4) Check server with an empty password for root or anonymous \n (5) Attempt to list all databases on server \n (6) Dumps the password hashes from an MySQL server \n (7) Show all whichiables on server \n (8) Audits MySQL database server security configuration \n     against parts of the CIS MySQL v1.0.2 benchmark \n     (the engine can be used for other MySQL audits \n     by creating appropriate audit files). \n (9) Run query against database \n (10) Attempt to bypass authentication exploiting CVE2012-2122 \n (11) Perform password guessing against MySQL \n (0) Exit \n (s) Back  \n\n")
+				print ("\n (1) Get info \n (2) List all users on server \n (3) Perform valid-user enumeration \n (4) Check server with an empty password for root or anonymous \n (5) Attempt to list all databases on server \n (6) Dumps the password hashes from an MySQL server \n (7) Show all whichiables on server \n (8) Audits MySQL database server security configuration \n     against parts of the CIS MySQL v1.0.2 benchmark \n     (the engine can be used for other MySQL audits \n     by creating appropriate audit files). \n (9) Run query against database \n (10) Attempt to bypass authentication exploiting CVE2012-2122 \n (11) Perform password guessing against MySQL \n (0) Exit \n (b) Back  \n\n")
 				print ("\n\n  RUN > ")
 				runs= gets().chomp()		
 				
@@ -437,7 +448,7 @@ $lin="==========================================================================
 						system ("NMap -p#{@rports} #{@opts} --script=mysql-brute #{@rhosts}")
 						mysql
 		
-					elsif runs == "start" || runs == "s"
+					elsif runs == "back" || runs == "b"
 		
 						which
 					
